4ff0fbb438a093c1d379401d31d610b9e4c60918
Gergely Imreh <imrehg@gmail.com>
2012-03-31 22:27:08 +0800

    add basic testserver

---
+++ helper/testserver.py
@@ -0,0 +1,30 @@
+from bottle import route, run, response
+import bottle
+import sys
+import json
+
+
+def loadapi(file):
+    ''' Helper to load files '''
+    datafile = open(sys.argv[1])
+    datatxt = "".join([line.strip() for line in datafile.readlines()])
+    print datatxt
+    datajson = json.loads(datatxt)
+
+@route('/api/<filename>')
+def index(filename=''):
+    datafile = open("./api/"+filename)
+    datatxt = "".join([line.strip() for line in datafile.readlines()])
+    datajson = json.loads(datatxt)
+    return json.dumps(datajson)
+
+@route('/static/:filename#.*#')
+def static(filename):
+    return bottle.static_file(filename, root='./static/')
+
+@route('/:filename#.*#')
+def static(filename):
+    return bottle.static_file(filename, root='./site/')
+
+
+run(host='localhost', port=8080)
